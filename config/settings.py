69f9ce865fb337ff215757b62810efcb164b790f
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-09-21 15:22:19 -0700

    feat(config): add the created apps in config app

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'users',
+    'medications',
+    'reminders',
+    'shared_access',
 ]
 
 #Cors añadido
