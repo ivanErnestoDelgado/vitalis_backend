f088c2d777dadb46072ae4c9c4c4e330a34c2731
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-10-25 18:32:28 -0700

    feat(medications): register a few models in the admin file for a administration for it in the admin panel

--- medications/admin.py
+++ medications/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Drug)
+class DrugAdmin(admin.ModelAdmin):
+    list_display=('description',)
+
+@admin.register(DrugVariant)
+class DrugVariantAdmin(admin.ModelAdmin):
+    list_display=('drug','variant_name','dosage')
+
+@admin.register(UnsafeMedication)
+class UnsafeMedicationsAdmin(admin.ModelAdmin):
+    list_display=('drug','patient','reason')
